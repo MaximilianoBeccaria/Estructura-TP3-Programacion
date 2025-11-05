fecdaa19b7c5ca1d77c49944745d677fceb0901f
Leonardou78 <leonardomontagna2020@gmail.com>
2025-11-05 17:10:03 -0300

    commit

---
+++ Tp3-Programacion/Menu Consola/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tp3_Programacion.Models;
+
+Console.WriteLine("Hello, World!");
+
+
---
+++ Tp3-Programacion/Tp3-Programacion/Repository/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp3_Programacion.Repository
+{
+    public static class Class1
+    {
+
+    }
+}
